f6cb7eadc979828faa84c2019a24b6a548103f46
yangtengjiao21@gmail.com <yangtengjiao21@gmail.com>
2022-01-09 14:51:06 +0800

    代码调整

--- nacos-provider/src/main/java/com/tenmue/micro/TestController.java
+++ nacos-provider/src/main/java/com/tenmue/micro/TestController.java
@@ -1,5 +1,6 @@
 package com.tenmue.micro;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,17 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-	@Value("${name}")
-	private String name;
+    @Value("${name}")
+    private String name;
 
-	@RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
-	public String echo(@PathVariable String string) {
-		System.out.println("value is"+name);
-		return "Hello Nacos Discovery " + string;
-	}
-	
-	@GetMapping("/hello")
-	public String hello() {
-		return "hello demo";
-	}
+    @Autowired
+    private UserInfo userInfo;
+
+    @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+    public String echo(@PathVariable String string) {
+        System.out.println("value is" + name);
+        System.out.println(userInfo.getUsername() + "" + userInfo.getPassword());
+        return "Hello Nacos Discovery " + string;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello demo";
+    }
 }
